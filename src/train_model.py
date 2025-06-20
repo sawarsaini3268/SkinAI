e61a262268e671240020c2503f27a10bea9a83f0
Sawar Saini <sawar26saini@gmail.com>
2025-06-19 17:40:06 -0700

    imports

---
+++ src/train_model.py
@@ -0,0 +1,7 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
