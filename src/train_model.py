05a189b36ae557cc60b1cb04b06b9d6544a67536
Sawar Saini <sawar26saini@gmail.com>
2025-06-21 12:07:19 -0700

    started config

--- src/train_model.py
+++ src/train_model.py
@@ -1,3 +1,4 @@
+#imports
 import os
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -5,3 +6,9 @@ from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam
+
+#configuration
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+EPOCHS = 5
+
